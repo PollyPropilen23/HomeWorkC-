f90a64646a405c6928f1845a0e20d3464a815a50
Polly <pollysha23@gmail.com>
2022-08-29 23:30:23 +0300

    third task of HW3 added

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -1,4 +1,5 @@
-﻿// Задача 1. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+﻿/*
+// Задача 1. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 int FindPolindrom (int num)
 {   int polindrom = 0;
     while (num >= 10)
@@ -19,3 +20,23 @@ if (num == res)
 Console.Write($"Число {num} полиндром");
 else
 Console.Write($"Число {num} не полиндром");
+*/
+
+/*
+// Задача 3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
+
+void Cube (int num)
+{
+    int current = 1;
+    while (current <= num)
+    {
+       int cube = current * current * current;
+       Console.Write (cube + " ");
+       current++;
+    }
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Cube(num);
+*/
