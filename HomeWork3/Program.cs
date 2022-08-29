cf68309162afe9e91f06ad7b3fd1b857769dc737
Polly <pollysha23@gmail.com>
2022-08-29 23:15:17 +0300

    fist task of HW3 added

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 1. Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+int FindPolindrom (int num)
+{   int polindrom = 0;
+    while (num >= 10)
+    {
+        polindrom = polindrom *10 + num % 10;
+        num = num/10;
+    }
+    polindrom = polindrom * 10 + num;
+    return polindrom;
+}   
+
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int res = FindPolindrom(num);
+
+if (num == res)
+Console.Write($"Число {num} полиндром");
+else
+Console.Write($"Число {num} не полиндром");
