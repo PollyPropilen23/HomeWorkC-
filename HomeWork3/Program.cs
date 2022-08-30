274b5621c7011cca003d91554f7668d776300f3e
Polly <pollysha23@gmail.com>
2022-08-30 17:36:52 +0300

    second task of HW3 added

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -23,6 +23,39 @@ Console.Write($"Число {num} не полиндром");
 */
 
 /*
+// Задача 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double Distance (double xa, double ya, double za, double xb, double yb, double zb)
+{
+    double sumQuad = Math.Pow((xb-xa),2) + Math.Pow((yb-ya),2) + Math.Pow((zb-za),2);
+    double dist = Math.Sqrt(sumQuad);
+    return dist;
+}
+
+Console. WriteLine ("Введите значение х для точки А");
+double xa = Convert.ToDouble(Console.ReadLine());
+
+Console. WriteLine ("Введите значение y для точки А");
+double ya = Convert.ToDouble(Console.ReadLine());
+
+Console. WriteLine ("Введите значение z для точки А");
+double za = Convert.ToDouble(Console.ReadLine());
+
+Console. WriteLine ("Введите значение х для точки B");
+double xb = Convert.ToDouble(Console.ReadLine());
+
+Console. WriteLine ("Введите значение y для точки B");
+double yb = Convert.ToDouble(Console.ReadLine());
+
+Console. WriteLine ("Введите значение z для точки B");
+double zb = Convert.ToDouble(Console.ReadLine());
+
+double dist = Distance(xa,ya,za,xb,yb,zb);
+
+Console.Write("Расстоние от точки А до точки В равно " + dist);
+*/
+
+/*
 // Задача 3. Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N
 
 void Cube (int num)
@@ -30,7 +63,7 @@ void Cube (int num)
     int current = 1;
     while (current <= num)
     {
-       int cube = current * current * current;
+       double cube = Math.Pow(current,3);
        Console.Write (cube + " ");
        current++;
     }
@@ -39,4 +72,28 @@ void Cube (int num)
 Console.Write("Введите число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 Cube(num);
+*/
+
+
+// Классаная задача
+/*
+void chetvert (int nom)
+{
+    if (nom == 1)
+    Console.WriteLine("В первой четверти значения по оси абсцис и ординат больше нуля");
+    if (nom == 2)
+    Console.WriteLine("Во второй четверти значения по оси абсцис меньше нуля, по оси ординат больше нуля");
+    if (nom == 3)
+    Console.WriteLine("В третьей четверти значения по оси абсцис и ординат меньше нуля");
+    if (nom == 4)
+    Console.WriteLine("В четвертой четверти значения по оси абсцис больше нуля, а по оси ординат меньше нуля");
+    if (nom > 4 || nom < 1)
+    Console.WriteLine("Нет четверти, соответствующей введенному числу");
+
+}
+
+Console.Write("Введите номер четверти ");
+int nom = Convert.ToInt32(Console.ReadLine());
+
+chetvert(nom);
 */
